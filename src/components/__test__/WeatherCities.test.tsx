d1c98b45c08d2096e1285865c90a8feffdc29a2e
Appukudumula <kudumulaaparna@gmail.com>
2021-03-16 13:27:14 +1100

    Add unit tests for WeatherCities Component

---
+++ src/components/__test__/WeatherCities.test.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import WeatherCities from '../WeatherCities';
+import { toEditorSettings } from 'typescript';
+
+describe('WeatherCities', () => {
+
+    test('Should render component component', () => {
+        const cities = ['Melbourne', 'Sydney', ''];
+        const mockFn = jest.fn();
+        render(<WeatherCities cities={cities} selectedCity='Melbourne' onCitySelection={mockFn} />);
+    });
+
+    test('Should render cities passed as props', () => {
+        const cities = ['Melbourne', 'Sydney', ''];
+        const mockFn = jest.fn();
+        render(<WeatherCities cities={cities} selectedCity='Melbourne' onCitySelection={mockFn} />);
+        const ele = screen.getByTestId('Melbourne_0');
+        expect(ele).toBeInTheDocument();
+        expect(ele.textContent).toBe('Melbourne');
+    });
+
+    test('Should call the onCitySelection function on city click', () => {
+        const cities = ['Melbourne', 'Sydney', ''];
+        const mockFn = jest.fn();
+        render(<WeatherCities cities={cities} selectedCity='Melbourne' onCitySelection={mockFn} />);
+        const ele = screen.getByTestId('Melbourne_0');
+        fireEvent.click(ele);
+        expect(mockFn).toHaveBeenCalledTimes(1);
+    });
+
+});
